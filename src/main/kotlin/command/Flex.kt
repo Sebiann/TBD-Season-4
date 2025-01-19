a5eef63efe9de9ad64b66032412862a16642606a
Austin Albrecht <austin.aa@me.com>
2025-01-19 20:45:13 +0100

    Add flex command

---
+++ src/main/kotlin/command/Flex.kt
@@ -0,0 +1,30 @@
+package command
+
+import io.papermc.paper.command.brigadier.CommandSourceStack
+import net.kyori.adventure.text.minimessage.MiniMessage
+import org.bukkit.Bukkit
+import org.bukkit.entity.Player
+import org.incendo.cloud.annotations.Command
+import org.incendo.cloud.annotations.Permission
+import org.incendo.cloud.annotations.processing.CommandContainer
+
+@Suppress("unused", "unstableApiUsage")
+@CommandContainer
+class Flex {
+    private val mm = MiniMessage.miniMessage()
+
+    @Command("flex")
+    @Permission("tbdseason4.command.flex")
+    fun echo(css: CommandSourceStack) {
+        val player = css.sender as Player
+        val itemStack = player.inventory.itemInMainHand
+        if (itemStack.isEmpty) {
+            player.sendMessage("You are not holding an item.\n\nIdiot...\n")
+        } else {
+            Bukkit.getServer().sendMessage(
+                mm.deserialize("${player.name} shows off ")
+                    .append(itemStack.effectiveName().hoverEvent(itemStack))
+            )
+        }
+    }
+}
