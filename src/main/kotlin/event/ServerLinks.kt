d5189d2d8b2b8ddd8cb5334021e660ef29d6b8cc
Austin Albrecht <austin.aa@me.com>
2025-01-19 18:46:06 +0100

    Improve server-links

--- src/main/kotlin/event/ServerLinks.kt
+++ src/main/kotlin/event/ServerLinks.kt
@@ -1,6 +1,7 @@
 package event
 
 import net.kyori.adventure.text.Component
+import net.kyori.adventure.text.minimessage.MiniMessage
 import org.bukkit.event.EventHandler
 import org.bukkit.event.Listener
 import org.bukkit.event.player.PlayerLinksSendEvent
@@ -8,9 +9,25 @@ import java.net.URI
 
 
 class ServerLinks : Listener {
+    private val mm = MiniMessage.miniMessage()
+    private val links: List<ServerLinkEntry> = listOf(
+        ServerLinkEntry(
+            mm.deserialize("<rainbow>made you look lol</rainbow>"),
+            URI("https://links.tbdsmp.net/madeyoulook")
+        ),
+        ServerLinkEntry(
+            mm.deserialize("<b><u><dark_red>BIGRAT DOT SOCIAL</dark_red></u></b>"),
+            URI("https://bigrat.social")
+        ),
+    )
+
     @EventHandler
     @Suppress("UnstableApiUsage")
     fun onPlayerLinksSendEvent(event: PlayerLinksSendEvent) {
-        event.links.addLink(Component.text("made you look"), URI("https://www.youtube.com/watch?v=dQw4w9WgXcQ"))
+        links.forEach {
+            event.links.addLink(it.component, it.uri)
+        }
     }
 }
+
+data class ServerLinkEntry(val component: Component, val uri: URI)
