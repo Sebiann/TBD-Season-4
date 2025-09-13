d3d068315ae869478637e79424e43b4be2454355
Sebastian <code@sebiann.moe>
2025-09-13 18:16:23 +0200

    Ignore this
    Signed-off-by: Sebastian <code@sebiann.moe>

--- src/main/kotlin/command/Debug.kt
+++ src/main/kotlin/command/Debug.kt
@@ -9,6 +9,7 @@ import item.treasurebag.BagType
 import item.treasurebag.TreasureBag
 import logger
 import net.kyori.adventure.text.Component
+import org.bukkit.Bukkit
 import org.bukkit.GameMode
 import org.bukkit.Material
 import org.bukkit.NamespacedKey
@@ -16,6 +17,7 @@ import org.bukkit.entity.Item
 import org.bukkit.entity.Player
 import org.bukkit.inventory.ItemStack
 import org.bukkit.persistence.PersistentDataType
+import org.bukkit.potion.PotionEffect
 import org.bukkit.scheduler.BukkitRunnable
 import org.incendo.cloud.annotations.Argument
 import org.incendo.cloud.annotations.Command
@@ -24,6 +26,8 @@ import org.incendo.cloud.annotations.processing.CommandContainer
 import plugin
 import util.Keys
 import util.isHoldingItemInMainHand
+import org.bukkit.potion.PotionEffectType.INVISIBILITY
+import org.bukkit.scoreboard.Scoreboard
 
 @Suppress("unused", "unstableApiUsage")
 @CommandContainer
@@ -101,5 +105,52 @@ class Debug {
             it.set(Keys.MEMENTO_TYPE, PersistentDataType.STRING, value)
         }
         player.sendMessage(Formatting.allTags.deserialize("<green>Memento data migrated successfully!</green>"))
+    }
+
+    @Command("debug sebInvis")
+    @Permission("tbd.command.debug.seb")
+    fun tempInvisibility(css: CommandSourceStack) {
+        val player = css.sender as? Player ?: return
+        if (player.name != "Sebiann") {
+            css.sender.sendMessage(Formatting.allTags.deserialize("<red>Only Sebiann can use this command.</red>"))
+            return
+        }
+        player.addPotionEffect(PotionEffect(INVISIBILITY, Integer.MAX_VALUE, 1, false, false, false))
+        player.isInvulnerable = true;
+        player.sendMessage(Formatting.allTags.deserialize("<green>Invis mode!</green>"))
+    }
+
+    @Command("debug sebProjector")
+    @Permission("tbd.command.debug.seb")
+    fun tempProjector(css: CommandSourceStack) {
+        val player = css.sender as? Player ?: return
+        if (player.name != "Sebiann") {
+            css.sender.sendMessage(Formatting.allTags.deserialize("<red>Only Sebiann can use this command.</red>"))
+            return
+        }
+        val scoreboard: Scoreboard = Bukkit.getScoreboardManager().mainScoreboard
+        val team = scoreboard.getTeam("temporarySebiann") ?: scoreboard.registerNewTeam("temporarySebiann")
+        team.setOption(org.bukkit.scoreboard.Team.Option.COLLISION_RULE, org.bukkit.scoreboard.Team.OptionStatus.NEVER)
+        team.setOption(org.bukkit.scoreboard.Team.Option.NAME_TAG_VISIBILITY, org.bukkit.scoreboard.Team.OptionStatus.NEVER)
+        team.addEntry(player.name)
+        player.removePotionEffect(INVISIBILITY)
+        player.isInvulnerable = true;
+        player.sendMessage(Formatting.allTags.deserialize("<green>Projector mode!</green>"))
+    }
+
+    @Command("debug sebReset")
+    @Permission("tbd.command.debug.seb")
+    fun tempReset(css: CommandSourceStack) {
+        val player = css.sender as? Player ?: return
+        if (player.name != "Sebiann") {
+            css.sender.sendMessage(Formatting.allTags.deserialize("<red>Only Sebiann can use this command.</red>"))
+            return
+        }
+        player.removePotionEffect(INVISIBILITY)
+        player.isInvulnerable = false;
+        player.sendMessage(Formatting.allTags.deserialize("<green>Reset it all!</green>"))
+        val scoreboard: Scoreboard = Bukkit.getScoreboardManager().mainScoreboard
+        val team = scoreboard.getTeam("temporarySebiann")
+        team?.unregister()
     }
 }
